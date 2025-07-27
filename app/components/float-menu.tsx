d104e99d3a84e3ac47aa4fa35a91346d21bbbc4c
xtnbyybs <2111803140@qq.com>
2025-07-28 00:09:26 +0800

    Update float-menu.tsx

--- app/components/float-menu.tsx
+++ app/components/float-menu.tsx
@@ -19,7 +19,7 @@ export function FloatMenu() {
               variant="outline"
               size="icon"
               className="bg-white dark:bg-background rounded-full shadow-lg group relative border-primary/20"
-              onClick={() => window.open("https://github.com/beilunyang/moemail", "_blank")}
+              onClick={() => window.open("https://github.com/xtnbyybs", "_blank")}
             >
               <Github 
                 className="w-4 h-4 transition-all duration-300 text-primary group-hover:scale-110"
