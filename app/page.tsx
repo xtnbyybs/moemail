f4e5daf9d376136d8a576548bc06934816e37c58
xtnbyybs <2111803140@qq.com>
2025-07-30 10:19:06 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,11 +21,11 @@ export default async function Home() {
               <div className="space-y-4">
                 <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold tracking-wider">
                   <span className="bg-clip-text text-transparent bg-gradient-to-r from-primary to-purple-600">
-                    MoeMail
+                    xtbymail
                   </span>
                 </h1>
                 <p className="text-lg sm:text-xl text-gray-600 dark:text-gray-300 tracking-wide">
-                  汤同学-临时邮箱服务
+                  汤同学-临时邮箱tg:xtbymail
                 </p>
               </div>
 
